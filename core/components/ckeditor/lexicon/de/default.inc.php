40908351f1cbfc224443239d619f8ae5b9f56c01
Dustin Kummer <miduku11@gmail.com>
2013-02-20 21:43:02 +0100

    German translation
    Added German translation for Settings of CKEditor

---
+++ core/components/ckeditor/lexicon/de/default.inc.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Default Language file for CKEditor
+ *
+ * @package ckeditor
+ * @subpackage lexicon
+ */
+
+$_lang['ckeditor'] = 'CKEditor';
+
+$_lang['area_general'] = 'Allgemeine Einstellungen';
+
+$_lang['setting_ckeditor.ui_color'] = 'UI-Farbe';
+$_lang['setting_ckeditor.ui_color_desc'] = 'Hexadezimalwert für die UI-Farbe. Zum Beispiel: «#328000» .';
+$_lang['setting_ckeditor.skin'] = 'Editor-Skin';
+$_lang['setting_ckeditor.skin_desc'] = 'Name des Editor-Skins. Ein skin muss im Verzeichnis «{manager_path}assets/components/ckeditor/ckeditor/skins/» liegen.';
+$_lang['setting_ckeditor.toolbar'] = 'Werkzeugleiste';
+$_lang['setting_ckeditor.toolbar_desc'] = 'Konfiguration der Werkzeugleiste im JSON-Format. Zum Beispiel: «[ [ "Cut", "Copy", "Paste", "PasteText", "PasteFromWord", "-", "Undo", "Redo" ], "/", { "name": "basicstyles", "items": [ "Bold", "Italic" ] } ]».';
+$_lang['setting_ckeditor.extra_plugins'] = 'Extra-Plugins';
+$_lang['setting_ckeditor.extra_plugins_desc'] = 'Liste von Plugins, getrennt mit «,». Alle Plugins müssen im Verzeichnis «{manager_path}/assets/components/ckeditor/ckeditor/plugins/» liegen.';
+$_lang['setting_ckeditor.object_resizing'] = 'Objekt-Größenänderung im Editor';
+$_lang['setting_ckeditor.object_resizing_desc'] = 'Objekt-Größenänderung im Editor aktivieren (Bilder, Tabellen).';
+
+$_lang['ui_ckeditor.insert_object_link'] = 'Objekt-Link einfügen';
+$_lang['ui_ckeditor.insert_object_name'] = 'Objekt-Name einfügen';
+$_lang['ui_ckeditor.insert_object'] = 'Objekt einfügen';
