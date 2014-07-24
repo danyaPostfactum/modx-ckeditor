c39114d47b539c48e4106e4275c3fab1faddf902
DanyaPostfactum <danya.postfactum@gmail.com>
2014-07-24 15:41:37 +1000

    Bump new version

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -12,8 +12,8 @@ set_time_limit(0);
 /* define version */
 define('PKG_NAME','CKEditor');
 define('PKG_NAMESPACE','ckeditor');
-define('PKG_VERSION','1.1.0');
-define('PKG_RELEASE','rc1');
+define('PKG_VERSION','1.1.1');
+define('PKG_RELEASE','pl');
 
 /* define sources */
 $root = dirname(dirname(__FILE__)).'/';
--- core/components/ckeditor/documents/readme.txt
+++ core/components/ckeditor/documents/readme.txt
@@ -1,8 +1,8 @@
 --------------------
 Extra: CKEditor
 --------------------
-Version: 1.1.0
-Created: December 27st, 2012
+Version: 1.1.1
+Created: April 2nd, 2014
 Since: December 5st, 2012
 Author: Danil Kostin <danya.postfactum@gmail.com>
 License: GNU GPLv2 (or later at your option)
